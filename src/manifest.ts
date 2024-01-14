ee84376fc255415e3ff76879719b0ff62cd2f48a
Duc-Minh Phan <alephvn@gmail.com>
2024-01-14 19:55:06 +0700

    feat: Set up skeleton

---
+++ src/manifest.ts
@@ -0,0 +1,31 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+
+import packageData from "../package.json";
+
+const isDev = process.env.NODE_ENV == "development";
+
+const name = "Coda Extension";
+
+export default defineManifest({
+  name: `${name}${isDev ? ` ➡️ Dev` : ""}`,
+  description: "Description",
+  version: packageData.version,
+  manifest_version: 3,
+  icons: {
+    16: "assets/icon/icon-16.png",
+    32: "assets/icon/icon-32.png",
+    48: "assets/icon/icon-48.png",
+    128: "assets/icon/icon-128.png",
+    256: "assets/icon/icon-256.png",
+  },
+  action: {
+    default_popup: "popup.html",
+    default_icon: "assets/icon/icon-48.png",
+  },
+  options_page: "options.html",
+  background: {
+    service_worker: "src/background.ts",
+    type: "module",
+  },
+  permissions: ["storage"],
+});
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+
+import manifest from "./src/manifest";
+
+// https://vitejs.dev/config/
+export default defineConfig(({}) => {
+  return {
+    build: {
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          chunkFileNames: "assets/chunk-[hash].js",
+        },
+      },
+    },
+
+    plugins: [
+      react(),
+      crx({
+        manifest,
+      }),
+    ],
+  };
+});
