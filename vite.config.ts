06d3dbdba5df086f415892360f28a21c2453afbf
Duc-Minh Phan <alephvn@gmail.com>
2024-01-20 10:59:30 +0700

    fix: Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import { crx } from "@crxjs/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 
 import manifest from "./src/manifest";
+
+const viteManifestHackIssue846: Plugin & {
+  renderCrxManifest: (manifest: any, bundle: any) => void;
+} = {
+  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
+  name: "manifestHackIssue846",
+  renderCrxManifest(_manifest, bundle) {
+    bundle["manifest.json"] = bundle[".vite/manifest.json"];
+    bundle["manifest.json"].fileName = "manifest.json";
+    delete bundle[".vite/manifest.json"];
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig(({}) => {
@@ -18,6 +30,7 @@ export default defineConfig(({}) => {
 
     plugins: [
       react(),
+      viteManifestHackIssue846,
       crx({
         manifest,
       }),
